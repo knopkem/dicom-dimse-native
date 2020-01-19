e28efcfe43235cceb0f6e4aaacd0119e81da6ace
Michael Knopke <knopkem@gmail.com>
2020-01-19 19:46:10 +0800

    prebuild-ci doesn't support cmake-js yet

--- index.js
+++ index.js
@@ -12,32 +12,34 @@ addon.startScp(JSON.stringify(
         console.log(result);
 });
 
-addon.moveScu(JSON.stringify(
-    {
-        "source": {
-            "aet": "IMEBRA",
-            "ip" : "127.0.0.1",
-            "port": "9999"
-        },
-        "target": {
-            "aet": "CONQUESTSRV1",
-            "ip" : "127.0.0.1",
-            "port": "5678"
-        },
-        "tags" : [
-            {
-                "key": "0020000D", 
-                "value": "1.3.46.670589.11.0.1.1996082307380006",
+setTimeout(function() {
+    addon.moveScu(JSON.stringify(
+        {
+            "source": {
+                "aet": "IMEBRA",
+                "ip" : "127.0.0.1",
+                "port": "9999"
             },
-            {
-                "key": "00080052", 
-                "value": "STUDY",
+            "target": {
+                "aet": "CONQUESTSRV1",
+                "ip" : "127.0.0.1",
+                "port": "5678"
             },
-        ]
-    }
-), (result) => {
-    console.log("result: ", result);
-});
+            "tags" : [
+                {
+                    "key": "0020000D", 
+                    "value": "1.3.46.670589.11.0.1.1996082307380006",
+                },
+                {
+                    "key": "00080052", 
+                    "value": "STUDY",
+                },
+            ]
+        }
+    ), (result) => {
+        console.log("result: ", result);
+    });
+}, 3000);
 
 addon.findScu(JSON.stringify(
     {
