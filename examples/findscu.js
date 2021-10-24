d9d5f7f020cde968fd2570bffeb2e9cfc3fc61ff
Michael Knopke <knopkem@gmail.com>
2021-10-24 08:11:40 +0200

    convert to ts (wip)

--- examples/findscu.js
+++ examples/findscu.js
@@ -1,140 +1,33 @@
-const addon = require('../index');
-
-addon.findScu(JSON.stringify(
-    {
-        "source": {
-            "aet": "DIMSE",
-            "ip" : "127.0.0.1",
-            "port": "9999"
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("../index");
+var options = {
+    source: {
+        aet: "DIMSE",
+        ip: "127.0.0.1",
+        port: "9999"
+    },
+    target: {
+        aet: "CONQUESTSRV1",
+        ip: "127.0.0.1",
+        port: "5678"
+    },
+    tags: [
+        {
+            key: "0020000D",
+            value: "1.3.46.670589.5.2.10.2156913941.892665384.993397",
         },
-        "target": {
-            "aet": "CONQUESTSRV1",
-            "ip" : "127.0.0.1",
-            "port": "5678"
+        {
+            key: "00080052",
+            value: "STUDY",
         },
-        "tags" : [
-            {
-                "key": "0020000D", 
-                "value": "1.3.46.670589.5.2.10.2156913941.892665384.993397",
-            },
-            {
-                "key": "00080052", 
-                "value": "STUDY",
-            },
-            {
-                "key": "00100010", 
-                "value": "",
-            },
-        ],
-        "verbose": true
-    }
-), (result) => {
-        if (result && result.length > 0) {
-            try
-            {
-                console.log(JSON.parse(result));
-                
-            }
-            catch (e) {
-                console.log(e, result);
-            }
+        {
+            key: "00100010",
+            value: "",
         }
-});
-
-addon.findScu(JSON.stringify(
-    {
-        "source": {
-            "aet": "DIMSE",
-            "ip" : "127.0.0.1",
-            "port": "9999"
-        },
-        "target": {
-            "aet": "CONQUESTSRV1",
-            "ip" : "127.0.0.1",
-            "port": "5678"
-        },
-        "tags" : [
-            {
-                "key": "0020000D", 
-                "value": "1.3.46.670589.5.2.10.2156913941.892665384.993397",
-            },
-            {
-                "key": "00080052", 
-                "value": "SERIES",
-            },
-            {
-                "key": "0020000E", 
-                "value": "",
-            },
-            {
-                "key": "00200010", 
-                "value": "",
-            },
-            {
-                "key": "00200011", 
-                "value": "",
-            },
-        ],
-        "verbose": true
-    }
-), (result) => {
-        if (result && result.length > 0) {
-            try
-            {
-                console.log(JSON.parse(result));
-                
-            }
-            catch (e) {
-                console.log(e, result);
-            }
-        }
-});
-
-addon.findScu(JSON.stringify(
-    {
-        "source": {
-            "aet": "DIMSE",
-            "ip" : "127.0.0.1",
-            "port": "9999"
-        },
-        "target": {
-            "aet": "CONQUESTSRV1",
-            "ip" : "127.0.0.1",
-            "port": "5678"
-        },
-        "tags" : [
-            {
-                "key": "0020000E", 
-                "value": "1.3.46.670589.5.2.10.2156913941.892665339.860724",
-            },
-            {
-                "key": "00080052", 
-                "value": "IMAGE",
-            },
-            {
-                "key": "00200012", 
-                "value": "",
-            },
-            {
-                "key": "00200013", 
-                "value": "",
-            },
-            {
-                "key": "00280103", 
-                "value": "",
-            },
-        ],
-        "verbose": true    
-    }
-), (result) => {
-        if (result && result.length > 0) {
-            try
-            {
-                console.log(JSON.parse(result));
-                
-            }
-            catch (e) {
-                console.log(e, result);
-            }
-        }
+    ],
+    verbose: true
+};
+index_1.findScu(options, function (result) {
+    console.log(result);
 });
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-module.exports = require('bindings')('dcmtk.node');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.parseFile = exports.shutdownScu = exports.startScp = exports.storeScu = exports.moveScu = exports.getScu = exports.findScu = exports.echoScu = void 0;
+var addon = require('bindings')('dcmtk.node');
+;
+;
+;
+;
+;
+;
+;
+;
+;
+function echoScu(options, callback) {
+    addon.echoScu(JSON.stringify(options), callback);
+}
+exports.echoScu = echoScu;
+function findScu(options, callback) {
+    addon.findScu(JSON.stringify(options), callback);
+}
+exports.findScu = findScu;
+function getScu(options, callback) {
+    addon.getScu(JSON.stringify(options), callback);
+}
+exports.getScu = getScu;
+function moveScu(options, callback) {
+    addon.moveScu(JSON.stringify(options), callback);
+}
+exports.moveScu = moveScu;
+function storeScu(options, callback) {
+    addon.storeScu(JSON.stringify(options), callback);
+}
+exports.storeScu = storeScu;
+function startScp(options, callback) {
+    addon.startScp(JSON.stringify(options), callback);
+}
+exports.startScp = startScp;
+function shutdownScu(options, callback) {
+    addon.shutdownScu(JSON.stringify(options), callback);
+}
+exports.shutdownScu = shutdownScu;
+function parseFile(options, callback) {
+    addon.parseFile(JSON.stringify(options), callback);
+}
+exports.parseFile = parseFile;
