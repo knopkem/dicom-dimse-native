2108bc081d6aeca066d5785636ad47f2d64ad2de
Michael Knopke <knopkem@gmail.com>
2024-01-26 10:56:24 +0100

    minor fix

--- examples/parse.ts
+++ examples/parse.ts
@@ -2,7 +2,7 @@ import { parseFile, parseOptions } from '../index';
 import p from 'path';
 
 const options: parseOptions = {
-    sourcePath: p.join(__dirname, 'dicom', 'sample.dcm'), // should point to single dicom file
+    sourcePath: p.join(__dirname, 'dicom', 'test.dcm'), // should point to single dicom file
     verbose: true,
 };
 
