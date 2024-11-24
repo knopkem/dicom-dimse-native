dc7138a7e7b6a01d653fd711ce8e323d2f9961a0
Michael Knopke <knopkem@gmail.com>
2024-11-24 10:42:32 +0100

    update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 "use strict";
-exports.__esModule = true;
-exports.recompress = exports.parseFile = exports.shutdownScu = exports.startStoreScp = exports.storeScu = exports.moveScu = exports.getScu = exports.findScu = exports.echoScu = void 0;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.echoScu = echoScu;
+exports.findScu = findScu;
+exports.getScu = getScu;
+exports.moveScu = moveScu;
+exports.storeScu = storeScu;
+exports.startStoreScp = startStoreScp;
+exports.shutdownScu = shutdownScu;
+exports.parseFile = parseFile;
+exports.recompress = recompress;
 var addon = require('bindings')('dcmtk.node');
 ;
 ;
@@ -14,36 +22,27 @@ var addon = require('bindings')('dcmtk.node');
 function echoScu(options, callback) {
     addon.echoScu(JSON.stringify(options), callback);
 }
-exports.echoScu = echoScu;
 function findScu(options, callback) {
     addon.findScu(JSON.stringify(options), callback);
 }
-exports.findScu = findScu;
 function getScu(options, callback) {
     addon.getScu(JSON.stringify(options), callback);
 }
-exports.getScu = getScu;
 function moveScu(options, callback) {
     addon.moveScu(JSON.stringify(options), callback);
 }
-exports.moveScu = moveScu;
 function storeScu(options, callback) {
     addon.storeScu(JSON.stringify(options), callback);
 }
-exports.storeScu = storeScu;
 function startStoreScp(options, callback) {
     addon.startScp(JSON.stringify(options), callback);
 }
-exports.startStoreScp = startStoreScp;
 function shutdownScu(options, callback) {
     addon.shutdownScu(JSON.stringify(options), callback);
 }
-exports.shutdownScu = shutdownScu;
 function parseFile(options, callback) {
     addon.parseFile(JSON.stringify(options), callback);
 }
-exports.parseFile = parseFile;
 function recompress(options, callback) {
     addon.recompress(JSON.stringify(options), callback);
 }
-exports.recompress = recompress;
